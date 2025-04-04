7e084661e13948b82dda0eb4c045fa991254684b
Łukasz Pęgiel <me@abapblog.com>
2025-04-04 09:51:30 +0200

    APE 1.0.8 freeze bug

---
+++ ABAP Project Extensions/src/com/abapblog/adt/extension/markers/MarkerDeletionJob.java
@@ -0,0 +1,36 @@
+package com.abapblog.adt.extension.markers;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+
+public class MarkerDeletionJob extends Job {
+
+	public static final String TASK_MARKER_ID = "com.abapblog.adt.extension.markers.task";
+	public static final String PROBLEM_MARKER_ID = "com.abapblog.adt.extension.markers.problem";
+	private IFile file;
+
+	public static void deleteMarkers(IFile file) {
+
+		MarkerDeletionJob lintingJob = new MarkerDeletionJob(file);
+		lintingJob.schedule();
+	}
+
+	private MarkerDeletionJob(IFile file) {
+		super("Delete ABAP Markers");
+		this.file = file;
+	}
+
+	@Override
+	protected Status run(IProgressMonitor monitor) {
+		try {
+			file.deleteMarkers(TASK_MARKER_ID, false, IFile.DEPTH_ZERO);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		return new Status(Status.OK, "com.abapblog.adt.extensions", "Markers deleted");
+	}
+
+}
